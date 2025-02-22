b9ef097ca6b64b7c621a12b107ff290587d835fa
Asher An <asher.an@cupix.com>
2025-02-22 14:54:32 +0900

    replace mesh geometry codes

--- src/geometry/Sphere.h
+++ src/geometry/Sphere.h
@@ -1,20 +1,58 @@
 #pragma once
 
+#include <gtc/matrix_transform.hpp>
+
 #include <vector>
 #include <vector_functions.h>
+#include <random>
 
 #include "../vector_math.h"
 
 class Sphere
 {
 public:
-	Sphere() { createSphere(); }
-	~Sphere() {}
+    Sphere()
+    {
+        createSphere();
+        setPosition();
+        setRotation();
+        setScale();
+    }
+    ~Sphere() {}
 
-	std::vector<float3>& getVertices() { return vertices; }
-	std::vector<unsigned int>& getIndices() { return indices; }
+    std::vector<float3>& getVertices() { return vertices; }
+    std::vector<unsigned int>& getIndices() { return indices; }
+
+    float3 getPosition() const { return position; }
+    float3 getRotation() const { return rotation; }
+    float3 getScale()    const { return scale; }
+    glm::mat4 getTransform() const {
+        float tx = position.x;
+        float ty = position.y;
+        float tz = position.z;
+
+        float yaw = rotation.x;
+        float pitch = rotation.y;
+        float roll = rotation.z;
+
+        glm::mat4 translation_mat = glm::translate(glm::mat4(1.0f), glm::vec3(tx, ty, tz));
+
+        glm::mat4 Ryaw = glm::rotate(glm::mat4(1.0f), yaw, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 Rpitch = glm::rotate(glm::mat4(1.0f), pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::mat4 Rroll = glm::rotate(glm::mat4(1.0f), roll, glm::vec3(0.0f, 0.0f, 1.0f));
+        glm::mat4 rotation_mat = Rroll * Rpitch * Ryaw;
+
+        glm::mat4 transform = translation_mat * rotation_mat;
+
+        return transform;
+    }
 
 private:
+    std::mt19937 gen{ std::random_device{}() };
+
+    std::uniform_real_distribution<float> randomPosition{ -0.5f, 0.5f };
+    std::uniform_real_distribution<float> randomAngle{ glm::radians(-30.0f), glm::radians(30.0f) };
+
     void createSphere()
     {
         // TODO: Hardcoded..
@@ -54,11 +92,12 @@ private:
 
                 // Unit sphere coordinates are the normals.
                 float3 normal = make_float3(cosPhi * sinTheta,
-                                            -cosTheta,           // -y to start at the south pole.
-                                            -sinPhi * sinTheta);
+                    -cosTheta,           // -y to start at the south pole.
+                    -sinPhi * sinTheta);
                 float3 vertex;
                 vertex = normal * radius;
                 vertices.push_back(vertex);
+				normals.push_back(normal);
             }
         }
 
@@ -81,6 +120,31 @@ private:
         }
     }
 
-	std::vector<float3>       vertices = {};
-	std::vector<unsigned int> indices  = {};
+    void setPosition()
+    {
+        position = make_float3(randomPosition(gen), randomPosition(gen), randomPosition(gen));
+    }
+
+    // Degrees
+    void setRotation()
+    {
+        float rot_x = glm::degrees(randomAngle(gen));
+        float rot_y = glm::degrees(randomAngle(gen));
+        float rot_z = glm::degrees(randomAngle(gen));
+
+        rotation = make_float3(rot_x, rot_y, rot_z);
+    }
+
+    void setScale()
+    {
+        scale = make_float3(1.0f, 1.0f, 1.0f);
+    }
+
+    std::vector<float3>       vertices = {};
+    std::vector<unsigned int> indices = {};
+    std::vector<float3>       normals = {};
+
+    float3 position = make_float3(0.0f, 0.0f, 0.0f);
+    float3 rotation = make_float3(0.0f, 0.0f, 0.0f);
+    float3 scale = make_float3(1.0f, 1.0f, 1.0f);
 };
